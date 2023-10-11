9b05073497dddbddb5ac927b4dddf8d3c716a5e6
mhida32 <117174272+mhida32@users.noreply.github.com>
2023-10-11 13:53:55 +1000

    Dz

---
+++ hioo.py
@@ -0,0 +1,53 @@
+my_dict = dict(name = '',age = 0, city = "")
+my_dict["name"] = input("Как тебя зовут?")
+my_dict["age"] = input("Сколько тебе лет?")
+my_dict["city"] = input("Откуда ты?")
+print(my_dict)
+
+
+
+
+
+
+# list = (4,5,6)
+# print(list)
+# print(f'Первое число: {list[0]}')
+# print(f'Второе число: {list[1]}')
+# print(f'третье число: {list[2]}')
+# for i in list:
+#     print(f'{list}')
+# print(f'Сумма: {sum(list)}')
+
+
+
+
+
+# n = int(input("Введите целое число: "))
+# i = 0
+# while i <= n:
+#     print(i)
+#     i += 1
+# print(f"i-{i},pot-{n}")
+
+
+
+
+
+
+
+
+
+
+# klava = int(input("Введи чисто от 1 до 10: "))
+# if klava % 3 == 0:
+#     print(f"Число делится на 3")
+# elif klava > 10:
+#     print("Число больше 10")
+# else:
+#     print("Число не соответствует условиям")
+
+
+
+# list = [1,2,"хуй",'хуй2', True, False,5,"tt",7,9]
+# print(list[1:10:2])
+# print(list[0:5])
