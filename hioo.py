25911f6eb20e33c70aabacfb8f1b8aaaba288366
mhida32 <117174272+mhida32@users.noreply.github.com>
2023-10-11 22:49:58 +1000

    hioo.py

--- hioo.py
+++ hioo.py
@@ -1,5 +1,7 @@
 #готовые дз только здесь. 
 # Позже везде пропишу комментарии
+
+# дз номер 9
 my_dict = dict(name = '',age = 0, city = "")
 my_dict["name"] = input("Как тебя зовут?")
 my_dict["age"] = input("Сколько тебе лет?")
@@ -10,28 +12,26 @@ print(my_dict)
 
 
 
-
-# list = (4,5,6)
-# print(list)
-# print(f'Первое число: {list[0]}')
-# print(f'Второе число: {list[1]}')
-# print(f'третье число: {list[2]}')
-# for i in list:
-#     print(f'{list}')
-# print(f'Сумма: {sum(list)}')
-
-
+# дз номер 8
+list = (4,5,6)
+print(list)
+print(f'Первое число: {list[0]}')
+print(f'Второе число: {list[1]}')
+print(f'третье число: {list[2]}')
+for i in list:
+    print(f'{list}')
+print(f'Сумма: {sum(list)}')
 
 
 
-# n = int(input("Введите целое число: "))
-# i = 0
-# while i <= n:
-#     print(i)
-#     i += 1
-# print(f"i-{i},pot-{n}")
 
-
+# дз номер 7
+n = int(input("Введите целое число: "))
+i = 0
+while i <= n:
+    print(i)
+    i += 1
+print(f"i-{i},pot-{n}")
 
 
 
@@ -40,16 +40,18 @@ print(my_dict)
 
 
 
-# klava = int(input("Введи чисто от 1 до 10: "))
-# if klava % 3 == 0:
-#     print(f"Число делится на 3")
-# elif klava > 10:
-#     print("Число больше 10")
-# else:
-#     print("Число не соответствует условиям")
+
+# дз номер 6
+klava = int(input("Введи чисто от 1 до 10: "))
+if klava % 3 == 0:
+    print(f"Число делится на 3")
+elif klava > 10:
+    print("Число больше 10")
+else:
+    print("Число не соответствует условиям")
 
 
-
-# list = [1,2,"хуй",'хуй2', True, False,5,"tt",7,9]
-# print(list[1:10:2])
-# print(list[0:5])
+# дз номер 5
+list = [1,2,"хуй",'хуй2', True, False,5,"tt",7,9]
+print(list[1:10:2])
+print(list[0:5])
