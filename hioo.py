f648e136c976b79de7d9fc57edeea92143d30723
mhida32 <117174272+mhida32@users.noreply.github.com>
2023-10-18 01:26:11 +1000

    hioo.py

--- hioo.py
+++ hioo.py
@@ -28,35 +28,35 @@ window.mainloop()
 
 
 # задание 8
-# my_dict = dict(name = '',age = 0, city = "")
-# my_dict["name"] = input("Как тебя зовут?")
-# my_dict["age"] = input("Сколько тебе лет?")
-# my_dict["city"] = input("Откуда ты?")
-# print(my_dict)
+my_dict = dict(name = '',age = 0, city = "")
+my_dict["name"] = input("Как тебя зовут?")
+my_dict["age"] = input("Сколько тебе лет?")
+my_dict["city"] = input("Откуда ты?")
+print(my_dict)
 
 
 
 
 # задание 7
-# list = (4,5,6)
-# print(list)
-# print(f'Первое число: {list[0]}')
-# print(f'Второе число: {list[1]}')
-# print(f'третье число: {list[2]}')
-# for i in list:
-#     print(f'{list}')
-# print(f'Сумма: {sum(list)}')
+list = (4,5,6)
+print(list)
+print(f'Первое число: {list[0]}')
+print(f'Второе число: {list[1]}')
+print(f'третье число: {list[2]}')
+for i in list:
+    print(f'{list}')
+print(f'Сумма: {sum(list)}')
 
 
 
 # задание 6
 
-# n = int(input("Введите целое число: "))
-# i = 0
-# while i <= n:
-#     print(i)
-#     i += 1
-# print(f"i-{i},pot-{n}")
+n = int(input("Введите целое число: "))
+i = 0
+while i <= n:
+    print(i)
+    i += 1
+print(f"i-{i},pot-{n}")
 
 
 
@@ -67,16 +67,16 @@ window.mainloop()
 
 
  # задание 5
-# klava = int(input("Введи чисто от 1 до 10: "))
-# if klava % 3 == 0:
-#     print(f"Число делится на 3")
-# elif klava > 10:
-#     print("Число больше 10")
-# else:
-#     print("Число не соответствует условиям")
+klava = int(input("Введи чисто от 1 до 10: "))
+if klava % 3 == 0:
+    print(f"Число делится на 3")
+elif klava > 10:
+    print("Число больше 10")
+else:
+    print("Число не соответствует условиям")
 
 
 
-# list = [1,2,"еей",'еее', True, False,5,"tt",7,9]
-# print(list[1:10:2])
-# print(list[0:5])
+list = [1,2,"еей",'еее', True, False,5,"tt",7,9]
+print(list[1:10:2])
+print(list[0:5])
