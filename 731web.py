20e5e8f0265f7dd74a413f62a4f0aa7f2fbe49a2
mhida32 <117174272+mhida32@users.noreply.github.com>
2023-10-18 13:46:52 +1000

    731web.py

--- 731web.py
+++ 731web.py
@@ -1,11 +1,17 @@
 from math import sqrt, pi
 print("Длины сторон треугольника:")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
-if a > 0 and b>0 and c>0 and a + b > c or a+c >b or b+c>a:
- p = (a + b + c) / 2
- s = sqrt(p * (p - a) * (p - b) * (p - c))
+a = input("a = ")
+b = input("b = ")
+c = input("c = ")
+if  a.isdigit() == True and  b.isdigit() == True and c.isdigit()== True:
+    af = float(a)
+    bf = float(b)
+    cf = float(c)
+    if af > 0 and bf>0 and cf>0 and af + bf > cf or af+cf >bf or bf+cf>af:
+        p = (af + bf + cf) / 2
+        s = sqrt(p * (p - af) * (p - bf) * (p - cf))
+        print("Площадь: %.2f" % s)
+    else:
+        print("error")
 else:
- print("error")
-print("Площадь: %.2f" % s)
+	print("error")
