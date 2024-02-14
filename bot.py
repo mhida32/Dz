5ddff283dec52454daa6776fbadc1e648690e25f
mhida32 <117174272+mhida32@users.noreply.github.com>
2024-02-15 04:19:40 +1000

    bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,18 @@
 credit by karpblch
 
 # Задание 1
-soon...
+import telebot
+from bot_token import TOKEN
+
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+    bot.send_message(message.chat.id, f"{(message.text * 10)} ")
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+    
 credit by karpblch
