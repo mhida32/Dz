96fa5955404ed429b811030bb8a5fadd0283b817
mhida32 <117174272+mhida32@users.noreply.github.com>
2024-02-15 04:23:45 +1000

    bot.py

--- bot.py
+++ bot.py
@@ -17,3 +17,27 @@ if __name__ == '__main__':
     bot.infinity_polling()
     
 credit by karpblch
+
+# Задание 2
+import telebot
+import random
+from bot_token import TOKEN
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(func=lambda message: "рандом" in message.text )
+def echo(message):
+    bot.send_message(message.chat.id, f"Случайное число: {random.randint(0,100)}")
+        
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id,message.text)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+
+credit by karpblch
